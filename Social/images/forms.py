0a912688388d3eac1e109788ecb30fbb317cbf65
talenhao <talenhao@gmail.com>
2017-07-25 18:04:45 +0800

    Overriding the save() method of a ModelForm

--- Social/images/forms.py
+++ Social/images/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from .models import Image
+from django.utils.text import slugify
+from urllib import request
+from django.core.files.base import ContentFile
 
 
 class ImageCreateForm(forms.ModelForm):
@@ -11,7 +14,7 @@ class ImageCreateForm(forms.ModelForm):
         }
 
     # forms.cleaned_data 对象
-    # clean_ < fieldname > ()
+    # clean_'field_name'()
     def clean_url(self):
         url = self.cleaned_data['url']
         valid_extensions = ['jpg', 'jpeg']
@@ -19,3 +22,20 @@ class ImageCreateForm(forms.ModelForm):
         if extension not in valid_extensions:
             raise forms.ValidationError('图片扩展名不正确!')
         return url
+
+    def save(self, commit=True, force_insert=False, force_update=False):
+        image = super(ImageCreateForm, self).save(commit=False)
+        image_url = self.cleaned_data['url']
+        image_name = "{}.{}".format(slugify(image.title),
+                                    image_url.rsplit('.', 1)[1].lower())
+        response = request.urlopen(image_url)
+        # image对象.image_field.save_method()
+        # ImageField inherits all attributes and methods from FileField,
+        # but also validates that the uploaded object is a valid image.
+        # image.FieldFile.save(name, content, save=True)
+        image.image.save(image_name,
+                         ContentFile(response.read()),
+                         save=False)
+        if commit:
+            image.save()
+        return image
--- Social/images/views.py
+++ Social/images/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth.decorators import login_required
+from .forms import ImageCreateForm
+from django.contrib import messages
+from django.shortcuts import redirect
+
+
+@login_required
+def image_create(request):
+    if request.method == "POST":
+        form = ImageCreateForm(data=request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            new_item = form.save(commit=False)
+            new_item.user = request.user
+            new_item.save()
+            messages.success('成功收藏图片!')
+            return redirect(new_item.get_absolute_url())
+    else:
+        form = ImageCreateForm(data=request.GET)
+
+    return render(request,
+                  "images/image/create.html",
+                  {"form": form,
+                   "section": "images"})
