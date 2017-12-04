7bf6367af24c8ea8f889ef14db95f3e3fd7c04fc
talenhao <talenhao@gmail.com>
2017-12-04 18:04:37 +0800

    20171204

--- myshop/shop/admin.py
+++ myshop/shop/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Category
+from .models import Product
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'stock', 'available', 'created', 'updated']
+    list_editable = ['stock', 'available', 'price']
+    list_filter = ['available', 'created', 'updated']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Product, ProductAdmin)
--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -20,3 +20,25 @@ class Category(models.Model):
 
 
 class Product(models.Model):
+    category = models.ForeignKey(Category,
+                                 related_name='products')  # 关联别名
+    name = models.CharField(max_length=200,
+                            db_index=True)
+    slug = models.SlugField(max_length=200,
+                            db_index=True)
+    image = models.ImageField(upload_to="products/$Y/$m/$d",
+                              blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10,
+                                decimal_places=2)
+    stock = models.PositiveIntegerField()  # 库存，正整数
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.name
